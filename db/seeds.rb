39af83eec11ffec4d45dbb1a63cfff61ff47b728
Jonatan Bonilla <pescadin138@gmail.com>
2018-11-24 08:04:34 -0500

    generate migration for user profiles and seeding with some basic profiles

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+ActiveRecord::Base.connection.execute <<-SQL
+    INSERT INTO profiles (profile) VALUES ('SuperAdministrador'), ('Administrador'), ('Ventas')
+SQL
